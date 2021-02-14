4dcae6293ebc35290bbc0110c48958167a408f32
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2021-02-13 22:09:23 -0500

    Updated

--- setup.bat
+++ setup.bat
@@ -1,5 +1,8 @@
 @echo off
-powershell Enable-PSRemoting
+netsh advfirewall firewall set rule group="Remote Scheduled Tasks Management" new enable=Yes
 
+echo Installing Moonlight-qt now... This should only take a minute...
+MoonlightSetup-3.0.0.exe /quiet
 
+echo Installation complete
 pause
